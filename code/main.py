6fe51ccbd1b54659e0a32bcc96049788e2f56842
tdutel <timothe.mister.mi@gmail.com>
2026-02-04 17:56:31 +0100

    implemented U D and R

--- code/main.py
+++ code/main.py
@@ -5,6 +5,15 @@ def main():
 	print("Rubik's cube created successfully!")
 	cube.display_in_term()
 	# cube.display_visual3D()
-
+	# cube.Up()
+	# cube.display_in_term()
+	# cube.Down()
+	# cube.display_in_term()
+	cube.Right()
+	cube.display_in_term()
+	# cube.Up()
+	# cube.display_in_term()
+	# cube.Right()
+	# cube.display_in_term()
 if __name__ == "__main__":
 	main()
--- code/rubik.py
+++ code/rubik.py
@@ -2,16 +2,23 @@ class RubiksCube:
 	def __init__(self):
 		# Initialize a solved cube (6 faces, each with 9 squares)
 		self.faces = {
-			'U': [['W']*3 for _ in range(3)],  # Up - White
-			'D': [['Y']*3 for _ in range(3)],  # Down - Yellow
-			'L': [['O']*3 for _ in range(3)],  # Left - Orange
-			'R': [['R']*3 for _ in range(3)],  # Right - Red
-			'F': [['G']*3 for _ in range(3)],  # Front - Green
-			'B': [['B']*3 for _ in range(3)]   # Back - Blue
+			# 'U': [['W'] * 3 for _ in range(3)],  # Up - White
+			# 'D': [['Y'] * 3 for _ in range(3)],  # Down - Yellow
+			# 'L': [['O'] * 3 for _ in range(3)],  # Left - Orange
+			# 'R': [['R'] * 3 for _ in range(3)],  # Right - Red
+			# 'F': [['G'] * 3 for _ in range(3)],  # Front - Green
+			# 'B': [['B'] * 3 for _ in range(3)]   # Back - Blue
+			'U': [['U1', 'U2', 'U3'], ['U4', 'U5', 'U6'], ['U7', 'U8', 'U9']],  # Up - White
+			'D': [['D1', 'D2', 'D3'], ['D4', 'D5', 'D6'], ['D7', 'D8', 'D9']],  # Down - Yellow
+			'L': [['L1', 'L2', 'L3'], ['L4', 'L5', 'L6'], ['L7', 'L8', 'L9']],  # Left - Orange
+			'R': [['R1', 'R2', 'R3'], ['R4', 'R5', 'R6'], ['R7', 'R8', 'R9']],  # Right - Red
+			'F': [['F1', 'F2', 'F3'], ['F4', 'F5', 'F6'], ['F7', 'F8', 'F9']],  # Front - Green
+			'B': [['B1', 'B2', 'B3'], ['B4', 'B5', 'B6'], ['B7', 'B8', 'B9']]   # Back - Blue
 		}
-	
+
 	def display_in_term(self):
 		"""Display the cube state"""
+		print("Current state of the Rubik's Cube:")
 		for face_name, face in self.faces.items():
 			print("\n" + face_name + " face:")
 			for row in face:
@@ -21,7 +28,90 @@ class RubiksCube:
 	def display_visual3D(self):
 		"""Placeholder for graphical display of the cube"""
 		print("Graphical display not implemented yet.")
+
+
+
+	def _deep_copy_faces(self):
+		"""Create a deep copy of all faces"""
+		copy = {}
+		for face_name, face in self.faces.items():
+			copy[face_name] = [row[:] for row in face]
+		return copy
+
 	# Additional methods for cube manipulation can be added here
+
+	def Up(self):
+		"""Rotate the upper face clockwise"""
+		face = self.faces
+		facecpy = self._deep_copy_faces()
+		
+		# Rotate the U face clockwise
+		face['U'][0][0] = facecpy['U'][2][0]
+		face['U'][0][1] = facecpy['U'][1][0]
+		face['U'][0][2] = facecpy['U'][0][0]
+
+		face['U'][1][0] = facecpy['U'][2][1]
+		face['U'][1][2] = facecpy['U'][0][1]
+		
+		face['U'][2][0] = facecpy['U'][2][2]
+		face['U'][2][1] = facecpy['U'][1][2]
+		face['U'][2][2] = facecpy['U'][0][2]
+		
+		# Cycle the edges (L, F, R, B faces)
+		face['L'][0] = facecpy['F'][0]
+		face['F'][0] = facecpy['R'][0]
+		face['R'][0] = facecpy['B'][0]
+		face['B'][0] = facecpy['L'][0]
+
+
+	def Down(self):
+		"""Rotate the down face clockwise"""
+		face = self.faces
+		facecpy = self._deep_copy_faces()
+		
+		# Rotate the D face clockwise
+		face['D'][0][0] = facecpy['D'][2][0]
+		face['D'][0][1] = facecpy['D'][1][0]
+		face['D'][0][2] = facecpy['D'][0][0]
+		
+		face['D'][1][0] = facecpy['D'][2][1]
+		face['D'][1][2] = facecpy['D'][0][1]
+		
+		face['D'][2][0] = facecpy['D'][2][2]
+		face['D'][2][1] = facecpy['D'][1][2]
+		face['D'][2][2] = facecpy['D'][0][2]
+		
+		# Cycle the edges (L, F, R, B faces)
+		face['L'][2] = facecpy['B'][2]
+		face['F'][2] = facecpy['L'][2]
+		face['R'][2] = facecpy['F'][2]
+		face['B'][2] = facecpy['R'][2]
+
+
+	def Right(self):
+		"""Rotate the right face clockwise"""
+		face = self.faces
+		facecpy = self._deep_copy_faces()
+		
+		# Rotate the R face clockwise
+		face['R'][0][0] = facecpy['R'][2][0]
+		face['R'][0][1] = facecpy['R'][1][0]
+		face['R'][0][2] = facecpy['R'][0][0]
+
+		face['R'][1][0] = facecpy['R'][2][1]
+		face['R'][1][2] = facecpy['R'][0][1]
+		
+		face['R'][2][0] = facecpy['R'][2][2]
+		face['R'][2][1] = facecpy['R'][1][2]
+		face['R'][2][2] = facecpy['R'][0][2]
+		
+		# Cycle the edges (L, F, R, B faces)
+		for i in range(3):
+			face['U'][i][2] = facecpy['F'][i][2]
+			face['F'][i][2] = facecpy['D'][i][2]
+			face['D'][i][2] = facecpy['B'][2 - i][0]
+			face['B'][2 - i][0] = facecpy['U'][i][2]
+
 
 
 	# def display(self):
